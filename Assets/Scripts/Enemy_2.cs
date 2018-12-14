4d3729970d37803f54bbe29ca7594107541d51ab
Jserna20 <31867660+Jserna20@users.noreply.github.com>
2018-12-14 10:32:13 -0500

    Enemy 2 created

--- Assets/Scripts/Enemy_2.cs
+++ Assets/Scripts/Enemy_2.cs
@@ -2,15 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_2 : MonoBehaviour {
+public class Enemy_2 : Enemy
+{
+    [Header("Set in Inspector: Enemy_2")]
+    public float sinEccentricity = 0.6f;
+    public float lifeTime = 10;
+
+    [Header("Set Dynamically: Enemy_2")]
+    public Vector3 p0;
+    public Vector3 p1;
+    public float birthTime;
+
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        p0 = Vector3.zero;
+        p0.x = -bndCheck.camWidth - bndCheck.radius;
+        p0.y = Random.Range(-bndCheck.camHeight, bndCheck.camHeight);
+
+        p1 = Vector3.zero;
+        p1.x = bndCheck.camWidth + bndCheck.radius;
+        p1.y = Random.Range(-bndCheck.camHeight, bndCheck.camHeight);
+
+        if(Random.value > 0.5)
+        {
+            p0.x *= -1;
+            p1.x *= -1;
+        }
+
+        birthTime = Time.time;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    public override void Move()
+    {
+        //Bezier curves work based on a u value between 0 & 1
+        float u = (Time.time - birthTime) / lifeTime;
+
+        if(u > 1)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        u = u + sinEccentricity * (Mathf.Sin(u * Mathf.PI * 2));
+
+        pos = (1 - u) * p0 + u * p1;
+
+        base.Move();
+    }
 }
